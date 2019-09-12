252b343f94c65b7ab32afb83f1c3bbda3a739b97
mixereqv1 <mateuszral7@gmail.com>
2019-09-12 18:34:30 +0200

    uploading...

---
+++ card.php
@@ -0,0 +1,70 @@
+<?php
+    $mysqli = new mysqli('localhost','root','','cars');
+
+    //CHECKING FOR ERROR WITH CONNECT
+    if($mysqli -> connect_errno) {
+        printf('Connect failed: %s\n', $mysqli -> connect_error);
+        exit();
+    }
+
+    $sql = 'SELECT * FROM cars';
+
+    //DEFINING ARRAYS
+    $name = array();
+    $description = array();
+    $price = array();
+    $promo = array();
+
+    //FETCHING DATA FROM DB TO ARRAYS
+    if($result = $mysqli ->query($sql)) {
+        while($row = $result -> fetch_assoc()) {
+            array_push($name,$row['nazwa']);
+            array_push($description,$row['opis']);
+            array_push($price,$row['cena']);
+            array_push($promo,$row['promocja']);
+        }
+    }
+
+    function checkI($i) {
+        $extension = '';
+        if($i == 0) {
+            $extension = 'png';
+        } else if($i == 4) {
+            $extension = 'jpeg';
+        } else {
+            $extension = 'jpg';
+        }
+        return $extension;
+    }
+
+    function createTab($i,$name,$description,$price,$promo) {
+        echo('
+            <div class="item card">
+                <div class="image">
+                    <img src="'.$name[$i].'.'.checkI($i).'" alt="'.$name[$i].'" class="image__img">
+                </div>
+                <div class="description">
+                    <span class="description__content">'.$description[$i].'</span>
+                    <div class="colors">
+                        <ul class="colors__list">
+                            Dostępne kolory:
+                            <li>Biały</li>
+                            <li>Niebieski</li>
+                            <li>Czerwony</li>
+                        </ul>
+                    </div>
+                    <div class="price">
+                        <span class="price__value">Cena: <span>'.$price[$i].'zł</span></span>
+                        <span class="price__promo">Promocja <span>-'.$promo[$i].'%</span></span>
+                    </div>
+                    <div class="buy">
+                        <a class="buy__link" href="#">Kup teraz!</a>
+                    </div>
+                    <div class="more">
+                        <a class="more__link" href="#">Zobacz więcej!</a>
+                    </div>
+                </div>
+            </div>
+        ');
+    }
+?>
